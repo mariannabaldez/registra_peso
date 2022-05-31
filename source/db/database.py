d713393a04949d975fe691a1bc5b00925feebb66
mariannabaldez <marianna.baldez@gmail.com>
2022-05-31 19:02:29 -0300

    conexao db com slqalchemy

--- source/db/database.py
+++ source/db/database.py
@@ -1,3 +1,4 @@
+from msilib import Table
 import sqlalchemy
 import databases
 
@@ -8,5 +9,12 @@ SQLALCHEMY_DATABASE_URL = "postgresql://postegres:development@localhost/registra
 database = databases.Database(SQLALCHEMY_DATABASE_URL)s
 metadata = sqlalchemy.MetaData()
 
+engine = sqlalchemy.create_engine(
+    SQLALCHEMY_DATABASE_URL,
+    execution_options={
+        "isolation_level": "REPEATABLE READ"
+    }
+)
 
 
+metadata.create_all(engine)
